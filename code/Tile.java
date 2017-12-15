f7605dbc359cfc2cd3de4ea0926607577f97cb37
Alphalfa <brianhung@berkeley.edu>
2017-12-15 15:09:22 -0800

    Update Tile.java

--- code/Tile.java
+++ code/Tile.java
@@ -1,14 +1,31 @@
 public Tile {
     
-    enum Type {
+    static enum Type {
         PROPERTY, SPECIAL, OTHER;
     }
     
     private Type type;
     
-    Queue<Player> player;
+    private Queue<Player> players;
     
-    public Tile() {
+    public Tile(Type _type) {
+        players = new Queue<>();
+        type = _type;
+    }
+    
+    // Moves a player from SRC and lands them at DST.
+    public static movePlayer(Tile src, Tile dst) {
+        dst.land(src.move());
+    }
+    
+    // Moves a player off a tile.
+    public Player move() {
+        return players.pop();
+    }
+    
+    // Lands a player onto a tile.
+    public void land(Player player) {
+        players.add(player);
     }
     
 }
