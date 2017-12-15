c1db8166657bf434e0ad801a8b3b78a22915ff72
Alphalfa <brianhung@berkeley.edu>
2017-12-15 15:37:19 -0800

    Update Board.java

--- code/Board.java
+++ code/Board.java
@@ -6,14 +6,40 @@ public Board {
     
     private int numPlayers;
     
+    private Player currentPlayer;
+    
+    private PlayerQueue queue;
+    
     private bool gameOver;
 
     public Board(int _numPlayers) {
-        numPlayers = _numPlayers
+        numPlayers = _numPlayers;
+        queue = initPlayers();
         gameOver = false;
     }
     
-    public int rollDice() {
+    private PlayerQueue initPlayers() {
+        // Prompts users for names.
+        Player[] players = requestPlayers();
+        return new PlayerQueue(players);
+    }
+    
+    public void makeMove() {
+        
+        currentPlayer = queue.curr(); 
+        
+        int currTile = currentPlayer.location();
+        int numMove  = currentPlayer.move();
+        
+        Tile.movePlayer(tile[currTile], tile[(currTile + numMove) % BOARD_LENGTH]);
+        currentPlayer.update(); currentPlayer.prompt(); currentPlayer = queue.next();
+    }
+    
+    public static int rollDice() {
         return (int) (Math.random() * 6 + 1);
+    }
+    
+    public void moveBackToParentsHouse(Player player) {
+        queue.dequeue(player);
     }
 }
